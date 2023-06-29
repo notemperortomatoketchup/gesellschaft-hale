7783531093f16a6294ef122bf1863725f0739f51
wotlk <sswotlk@gmail.com>
2023-06-29 18:42:45 +0200

    added errorCh into actions so that they don't hang if creation of page or deletion hang

--- client/actions.go
+++ client/actions.go
@@ -14,21 +14,26 @@ func actionExtractMails(b *Browser, w *protocol.Website) {
 	defer cancel()
 
 	successCh := make(chan struct{}, 1)
+	errorCh := make(chan error, 1)
 	go func() {
 		page, err := b.createPage()
 		if err != nil {
 			log.Printf("error creating page: %v", err)
+			errorCh <- err
 		}
 		stepExtractPaths(page, w, patterns)
 		stepExtractMails(page, w)
 		successCh <- struct{}{}
 		if err := page.Close(); err != nil {
 			log.Printf("error closing page: %v", err)
+			errorCh <- err
 		}
 	}()
 
 	select {
 	case <-successCh:
+		break
+	case <-errorCh:
 		break
 	case <-ctx.Done():
 		fmt.Println("context timed out for", w.BaseUrl)
